d5c4b219d7707abd3aff34f5ab94d26957e35ffb
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-01-07 23:54:27 +0100

    har lyckats få meddelande på UI

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
 import io from 'socket.io-client'
 
+
 class Chat extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            username: props.username,
+            messages: []
+        }
+    }
+    
+
+    componentDidMount() {
+        const socket = io('http://3.120.96.16:3000');
+        socket.on('messages', (data) => {
+
+        this.setState({
+            messages: data
+        })
+    })
+    }
+
+    componentDidUpdate() {
+        const socket = io('http://3.120.96.16:3000');
+        socket.on('messages', (data) => {
+
+        this.setState({
+            messages: data
+        })
+    })
+    }
+
+    clg = () => {
+        console.log(this.state.messages);
+    }
+
     render() {
-        let username = this.props.username;
-        const socket = io('http://3.120.96.16:3000')
+        let messages = this.state.messages.map(message => {
+            return <p>{message.username}: {message.content}</p>;
+        });
         return (
-            <div>
-                <p>{username}</p>
+            <div className="chat">
+                {messages}
             </div>
         )
     }
